662f8a07ed20e0a20f94e10fd918786f230f8708
shiva <shiva@pgi-amarendrad1.Prodigy.local>
2019-04-17 16:40:01 +0530

    DATE,TIME,TIMESTAMP

---
+++ src/main/java/com/example/demo/model/DATEandTIMESTAMP/DateEvent.java
@@ -0,0 +1,35 @@
+package com.example.demo.model.DATEandTIMESTAMP;
+
+import java.sql.Date;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class DateEvent {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;
+	@Temporal(TemporalType.DATE)
+	private Date date;
+	@Temporal(TemporalType.TIME)
+	private Date time;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date timestamp;
+}
